d0741b6c79ca8efebe7922513b55b6f5edddc23f
James <justjamescreative@outlook.com>
2025-04-23 23:14:10 -0400

    Added the RestController annotation and started the endpoints

--- src/main/java/com/coderscampus/assignment9/web/RecipeController.java
+++ src/main/java/com/coderscampus/assignment9/web/RecipeController.java
@@ -1,4 +1,22 @@
 package com.coderscampus.assignment9.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RecipeController {
+
+    @GetMapping("/gluten-free")
+// The “gluten-free” endpoint will only return a subset of the full Collection where glutenFree is true
+    @GetMapping("/vegan")
+// The “vegan” endpoint will only return a subset of the full Collection where vegan is true
+    @GetMapping("/vegan-and-gluten-free")
+// The “vegan-and-gluten-free” endpoint will only return a subset of the full Collection where glutenFree is true and
+// vegan is true
+    @GetMapping("/vegetarian")
+// The “vegetarian” endpoint will only return a subset of the full Collection where vegetarian is true
+    @GetMapping("/all-recipes")
+// The “all-recipes” endpoint will not filter the data at all and should return the full Collection.
+
+
 }
