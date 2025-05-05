5036a7d37e3e333975525620cd2df0e08b2757b9
James <justjamescreative@outlook.com>
2025-05-05 00:30:29 -0400

    Updated Assignment 9

--- src/main/java/com/coderscampus/assignment9/repository/RecipeRepository.java
+++ src/main/java/com/coderscampus/assignment9/repository/RecipeRepository.java
@@ -1,11 +1,8 @@
 package com.coderscampus.assignment9.repository;
 
-import com.coderscampus.assignment9.domain.Recipe;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface RecipeRepository {
-    Optional<Recipe> findByTitle(String title);
+public class RecipeRepository {
+
 }
--- src/main/java/com/coderscampus/assignment9/web/RecipeController.java
+++ src/main/java/com/coderscampus/assignment9/web/RecipeController.java
@@ -1,21 +1,48 @@
 package com.coderscampus.assignment9.web;
 
+import com.coderscampus.assignment9.domain.Recipe;
+import com.coderscampus.assignment9.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class RecipeController {
 
+    @Autowired
+    RecipeService recipeService;
+
+    @Autowired
+    List<Recipe> recipes;
+
     @GetMapping("/gluten-free")
+    public List<Recipe> getGlutenFree() {
+
+        return null;
+    }
 // The “gluten-free” endpoint will only return a subset of the full Collection where glutenFree is true
     @GetMapping("/vegan")
+    public List<Recipe> getVeganRecipes() {
+        return null;
+    }
 // The “vegan” endpoint will only return a subset of the full Collection where vegan is true
     @GetMapping("/vegan-and-gluten-free")
+    public List<Recipe> getVeganAndGlutenFreeRecipes() {
+        return null;
+    }
 // The “vegan-and-gluten-free” endpoint will only return a subset of the full Collection where glutenFree is true and
 // vegan is true
     @GetMapping("/vegetarian")
+    public List<Recipe> getVegetarianRecipes() {
+        return null;
+    }
 // The “vegetarian” endpoint will only return a subset of the full Collection where vegetarian is true
     @GetMapping("/all-recipes")
+    public List<Recipe> getAllRecipes() {
+        return recipes;
+    }
 // The “all-recipes” endpoint will not filter the data at all and should return the full Collection.
 
 
